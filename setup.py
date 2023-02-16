a10c3b0c507a4a426d95e0ddbe21510f93d6946f
suenghun <63846187+suenghun@users.noreply.github.com>
2023-02-16 12:17:19 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import find_packages, setup
 setup(
    name='UPMSP',
    version='1.0',
-   packages=find_packages(),  # would be the same as name
+   packages={'agent', 'environment'},  # would be the same as name
    )
